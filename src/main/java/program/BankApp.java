07589e8a404a40f619d028631c43131c817db621
Alexander Falk <alexanderfalk@Alexanders-MacBook-Pro.local>
2017-10-11 22:15:58 +0200

    Test push

--- src/main/java/program/BankApp.java
+++ src/main/java/program/BankApp.java
@@ -9,14 +9,6 @@ import java.util.*;
 @WebService
 public class BankApp {
 
-    /*private static int customerCreditScore = 300;
-    private static int customerLoanAmount = 75000;
-    private static double customerLoanDuration = 5;
-
-    public static void main(String[] args) {
-
-        System.out.println(getWSDLBank(customerCreditScore, customerLoanAmount, customerLoanDuration));
-    }*/
     private BankWSDL bank = new BankWSDL();
 
     public List<Object> getWSDLBank(int customerCreditScore, int customerLoanAmount, double loanDuration) {
